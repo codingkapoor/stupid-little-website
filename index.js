ab02360f753f1e0b6e0d1e305ff5ebeba1cbdf83
codingkapoor <mailtoshivamk@gmail.com>
2017-11-07 00:32:28 +0530

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+var koa = require("koa");
+var app = new koa();
+
+app.use(function *() {
+  this.body = "Koa says Hi!"
+});
+
+var port = process.env.PORT || (process.argv[2] || 3000);
+app.listen(port);
+console.log("Application started...");
